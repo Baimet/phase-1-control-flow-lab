4b8717853ed47467ab642555f64ab8036104d3ac
baimet <isaacbaimetyatich@gmail.com>
2024-07-12 13:15:48 +0300

    switch

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(addSomeValue) {
   // Write your code here!
+  if (addSomeValue <= 400) {
+    return "This one is on me!";
+  } else if (addSomeValue > 400 && addSomeValue <= 2000) {
+    return "That will be twenty bucks.";
+  } else if (addSomeValue > 2000 && addSomeValue <= 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else {
+    return "No can do.";
+  }
+}
+function ternaryCheckCity(string) {
+  // Write your code here!
+  if (string === "NYC") {
+    return "Ok, sounds good.";
+  } else {
+    return "No go.";
+  }
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tip) {
   // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+  switch (tip) {
+    case "generous":
+      return "Thank you so much.";
+    case "not as generous":
+      return "Thank you.";
+    default:
+      return "Bye.";
+  }
 }
